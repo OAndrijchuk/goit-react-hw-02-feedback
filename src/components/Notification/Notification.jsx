968c5df2a16d6fa362be81640e706b7cebb38acf
Oleh Andrijchuk <107946653+OAndrijchuk@users.noreply.github.com>
2023-07-30 11:46:28 +0300

    done

---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,9 @@
+export const Notification = ({message}) => { 
+
+    return (
+        <p>
+           {message} 
+        </p>
+        )
+
+}
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,18 @@
+export const Statistics = (props) => { 
+    const { good,
+        neutral,
+        bad,
+        total,
+        positivePercentage, } = props;
+    
+    return (
+        <div>
+            <p>Good: {good}</p>
+            <p>Neutral: {neutral}</p>
+            <p>Bad: {bad}</p>
+            <p>Total: {total()}</p>
+            <p>Positive feedback: {'0'&&positivePercentage()}%</p>
+        </div>
+        )
+
+}
